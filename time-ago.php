0b8290ff0e757ce722194b7f7fa6b68dfc4c10ae
Pamela Ribeiro <55361215+pamprn09@users.noreply.github.com>
2022-01-24 16:46:55 -0300

    Update time-ago.php

--- time-ago.php
+++ time-ago.php
@@ -1,19 +1,27 @@
-<?php 
+<?php
+
+/**
+ * Determine how long ago post was published.
+ *
+ * @param int $post_id ID of post.
+ * @return string Human-readable string describing age of post.
+ */
+
 function relative_time( $post_id ) {
-    $d1 = new \DateTime(get_the_date('c', $post_id));
-    $d2 = new \DateTime(current_time('c'));
-    $diff = $d2->diff($d1);
-    $d = $diff->d;
-    $h = $diff->h;
-    $m= $diff->m;
 
-    if ($d == 0) {
-        if ($h == 0) {
-            return "{$m}m ago";
-        }
-        return "{$h}h {$m}m ago";
-    }
+	$d1   = new \DateTime( get_the_date( 'c', $post_id ) );
+	$d2   = new \DateTime( current_time( 'c' ) );
+	$diff = $d2->diff( $d1 );
+	$d    = $diff->d;
+	$h    = $diff->h;
+	$m    = $diff->m;
 
-    return "{$d}d {$h}h {$m}m ago";
-    
+	if ( 0 === $d ) {
+		if ( 0 === $h ) {
+			return "{$m}m ago";
+		}
+		return "{$h}h {$m}m ago";
+	}
+
+	return "{$d}d {$h}h {$m}m ago";
 }
